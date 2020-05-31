55e439c118aa5bdd3a9217073b9fb81fd646a440
kbaikov <konstantin.baikov@gmail.com>
2020-05-31 10:05:06 +0200

    Solve day 5 part 2

--- day05.py
+++ day05.py
@@ -1,7 +1,9 @@
 import os
-from itertools import zip_longest, tee, filterfalse
+from itertools import tee
 import logging as log
 from pathlib import Path
+from collections import Counter, deque
+import string
 
 import pytest
 
@@ -48,7 +50,61 @@ def process_list(test_pattern):
     return result_string
 
 
+def process_list2(test_pattern):
+    c = Counter()
+    paired_list = list(pairwise(test_pattern))
+    result_string = test_pattern
+
+    while can_react(result_string):
+        for pair in pairwise(result_string):
+            if react(pair):
+                reacted_string = "".join(pair)
+                c[reacted_string.lower()] += 1
+                half1, _, half2 = result_string.partition(reacted_string)
+                result_string = f"{half1}{half2}"
+                break
+    return result_string, c
+
+
+def potential_pairs(pattern):
+    c = 0
+    for pair in pairwise(pattern):
+        if react(pair):
+            c += 1
+    return c
+
+
+def process_stack(pattern):
+    """Pop if reacts, add if not.
+
+    idea taken from: https://steadbytes.com/blog/advent-of-code-2018/05/"""
+    stack = deque()
+
+    for char in pattern:
+        if stack and react((char, stack[-1])):
+            stack.pop()
+        else:
+            stack.append(char)
+    return len(stack)
+
+
+def process_stack2(pattern):
+    """idea taken from: https://steadbytes.com/blog/advent-of-code-2018/05/"""
+    best = len(pattern)
+
+    for char in string.ascii_lowercase:
+        modified = pattern.replace(char, "")
+        modified = modified.replace(char.upper(), "")
+        modified_reacted_len = process_stack(modified)
+        best = min(best, modified_reacted_len)
+    return best
+
+
 if __name__ == "__main__":
+    # part 1: 11590
+    # part 2: 4504
     pattern = Path("day05_input.txt").read_text().strip()
-    result = process_list(pattern)
-    print(result, len(result))  # 11590
+    result1 = process_stack(pattern)
+    result2 = process_stack2(pattern)
+
+    print(result1, result2)
